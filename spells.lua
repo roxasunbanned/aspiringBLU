52b6ed682fc64faf46f2075a2a23bc3ec996da32
roxasunbanned <lvl93roxas@gmail.com>
2024-09-02 02:48:13 +0100

    Added zones.lua, spells.lua, BlueMagicAvailable.wav

---
+++ spells.lua
@@ -0,0 +1,343 @@
+return {
+    [549] = {
+        name = "Pollen",
+        mobs = {
+            {
+                monster = "Bumblebee",
+                level = "1",
+                zone = "West Sarutabaruta"
+            },
+            {
+                monster = "Bumblebee",
+                level = "1",
+                zone = "East Sarutabaruta"
+            },
+            {
+                monster = "Huge Hornet",
+                level = "1",
+                zone = "North Gustaberg"
+            },
+            {
+                monster = "Huge Hornet",
+                level = "1",
+                zone = "South Gustaberg"
+            },
+            {
+                monster = "Giddeus Bee",
+                level = "2-5",
+                zone = "Giddeus"
+            },
+            {
+                monster = "Maneating Hornet",
+                level = "2-5",
+                zone = "North Gustaberg"
+            },
+            {
+                monster = "Maneating Hornet",
+                level = "2-6",
+                zone = "South Gustaberg"
+            },
+            {
+                monster = "Giant Bee",
+                level = "5-8",
+                zone = "West Sarutabaruta"
+            },
+            {
+                monster = "Belaboring Wasp",
+                level = "100",
+                zone = "Ceizak Battlegrounds"
+            }
+        }
+    },
+    [577] = {
+        name = "Foot Kick",
+        mobs = {
+            {
+                monster = "Wild Rabbit",
+                level = "1",
+                zone = "East Ronfaure"
+            },
+            {
+                monster = "Wild Rabbit",
+                level = "1",
+                zone = "West Ronfaure"
+            },
+            {
+                monster = "Forest Hare",
+                level = "1-5",
+                zone = "East Ronfaure"
+            },
+            {
+                monster = "Forest Hare",
+                level = "1-5",
+                zone = "West Ronfaure"
+            },
+            -- Other entries...
+        }
+    },
+    [524] = {
+        name = "Sandspin",
+        mobs = {
+            {
+                monster = "Tunnel Worm",
+                level = "1",
+                zone = "South Gustaberg"
+            },
+            {
+                monster = "Tunnel Worm",
+                level = "1",
+                zone = "North Gustaberg"
+            },
+            {
+                monster = "Tunnel Worm",
+                level = "1-3",
+                zone = "Zeruhn Mines"
+            },
+            {
+                monster = "Carrion Worm",
+                level = "1-5",
+                zone = "West Ronfaure"
+            },
+            {
+                monster = "Carrion Worm",
+                level = "1-5",
+                zone = "East Ronfaure"
+            },
+            {
+                monster = "Carrion Worm",
+                level = "1-5",
+                zone = "West Ronfaure"
+            },
+            {
+                monster = "Carrion Worm",
+                level = "2-5",
+                zone = "King Ranperre's Tomb"
+            },
+            {
+                monster = "Stone Eater",
+                level = "2-5",
+                zone = "South Gustaberg"
+            },
+            {
+                monster = "Stone Eater",
+                level = "2-5",
+                zone = "North Gustaberg"
+            },
+            {
+                monster = "Stone Eater",
+                level = "3-5",
+                zone = "Dangruf Wadi"
+            },
+            {
+                monster = "Dirt Eater",
+                level = "3-5",
+                zone = "Giddeus"
+            },
+            {
+                monster = "Stone Eater",
+                level = "5-7",
+                zone = "King Ranperre's Tomb"
+            },
+            {
+                monster = "Rock Eater",
+                level = "7-11",
+                zone = "La Theine Plateau"
+            },
+            {
+                monster = "Rock Eater",
+                level = "7-11",
+                zone = "Konschtat Highlands"
+            },
+            {
+                monster = "Giant Grub",
+                level = "9-12",
+                zone = "Dangruf Wadi"
+            },
+            {
+                monster = "Earth Eater",
+                level = "10-12",
+                zone = "Giddeus"
+            },
+            {
+                monster = "Crepuscular Worm",
+                level = "103-105",
+                zone = "Cirdas Caverns"
+            }
+        }
+    },
+    [597] = {
+        name = "Sprout Smack",
+        mobs = {
+            {
+                monster = "Walking Sapling",
+                level = "3-7",
+                zone = "South Gustaberg"
+            },
+            {
+                monster = "Walking Sapling",
+                level = "3-7",
+                zone = "North Gustaberg"
+            },
+            {
+                monster = "Strolling Sapling",
+                level = "7-10",
+                zone = "La Theine Plateau"
+            },
+            {
+                monster = "Strolling Sapling",
+                level = "7-10",
+                zone = "Tahrongi Canyon"
+            },
+            {
+                monster = "Strolling Sapling",
+                level = "7-10",
+                zone = "Konschtat Highlands"
+            },
+            {
+                monster = "Stalking Sapling",
+                level = "18-21",
+                zone = "Batallia Downs"
+            },
+            {
+                monster = "Treant Sapling",
+                level = "66-68",
+                zone = "Wajaom Woodlands"
+            },
+            {
+                monster = "Trotting Sapling",
+                level = "74-84",
+                zone = "Abyssea - Konschtat"
+            }
+        }
+    },
+    [603] = {
+        name = "Wild Oats",
+        mobs = {
+            {
+                monster = "Tiny Mandragora",
+                level = "1",
+                zone = "West Sarutabaruta"
+            },
+            {
+                monster = "Tiny Mandragora",
+                level = "1",
+                zone = "East Sarutabaruta"
+            },
+            {
+                monster = "Mandragora",
+                level = "3-5",
+                zone = "West Sarutabaruta"
+            },
+            {
+                monster = "Mandragora",
+                level = "3-6",
+                zone = "East Sarutabaruta"
+            },
+            {
+                monster = "Pygmaioi",
+                level = "11-15",
+                zone = "Tahrongi Canyon"
+            },
+            {
+                monster = "Sylvestre",
+                level = "15-18",
+                zone = "Buburimu Peninsula"
+            },
+            {
+                monster = "Hannequet",
+                level = "80-95",
+                zone = "Abyssea - Attohwa"
+            },
+            {
+                monster = "Blanched Mandragora",
+                level = "100",
+                zone = "Ceizak Battlegrounds"
+            }
+        }
+    },
+    [551] = {
+        name = "Power Attack",
+        mobs = {
+            {
+                monster = "Scarab Beetle",
+                level = "3-6",
+                zone = "West Ronfaure"
+            },
+            {
+                monster = "Copper Beetle",
+                level = "9-12",
+                zone = "Palborough Mines"
+            },
+            {
+                monster = "Beady Beetle",
+                level = "11-16",
+                zone = "Outer Horutoto Ruins"
+            },
+            {
+                monster = "Beady Beetle",
+                level = "15-18",
+                zone = "Carpenters' Landing"
+            },
+            {
+                monster = "Stag Beetle",
+                level = "20-23",
+                zone = "Carpenters' Landing"
+            },
+            {
+                monster = "Stag Beetle",
+                level = "20-23",
+                zone = "Jugner Forest"
+            },
+            {
+                monster = "Stag Beetle",
+                level = "20-23",
+                zone = "Meriphataud Mountains"
+            },
+            {
+                monster = "Canopycrusher Beetle",
+                level = "100-101",
+                zone = "Yahse Hunting Grounds"
+            }
+        }
+    },
+    [547] = {
+        name = "Cocoon",
+        mobs = {
+            {
+                monster = "Crawler",
+                level = "3-8",
+                zone = "East Sarutabaruta"
+            },
+            {
+                monster = "Crawler",
+                level = "3-8",
+                zone = "West Sarutabaruta"
+            },
+            {
+                monster = "Canyon Crawler",
+                level = "11-13",
+                zone = "Tahrongi Canyon"
+            },
+            {
+                monster = "Carnivorous Crawler",
+                level = "20-23",
+                zone = "Pashhow Marshlands"
+            },
+            {
+                monster = "Worker Crawler",
+                level = "40-44",
+                zone = "Crawlers' Nest"
+            },
+            {
+                monster = "Eschan Crawler",
+                level = "52-59",
+                zone = "Escha - Zi'Tah"
+            },
+            {
+                monster = "Conflagrant Eruca",
+                level = "101-102",
+                zone = "Moh Gates"
+            }
+        }
+    }
+}
